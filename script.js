83bc0b8d6b75cb30f22b27dcf6adcf8be6b1fab2
Khan Imran <98589155+erkhanimran@users.noreply.github.com>
2023-07-12 15:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+'use strict';
+let out = prompt("Enter A String");
+function firstNonRepeatingCharacter(out) {
+  for (let i = 0; i < out.length; i++) {
+    let char = out[i];
+    if (out.indexOf(char) == i && out.indexOf(char, i + 1) == -1) {
+      return char;
+    }
+  }
+  return null;
+};
+let ans = firstNonRepeatingCharacter(out);
+alert(ans);
